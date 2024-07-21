df9e5de5b925af7caeca7b0063bd443f56d96e05
FrostedKaldr <lwilkins18@gmail.com>
2024-07-20 22:56:56 -0700

    config.h

--- config.h
+++ config.h
@@ -25,7 +25,78 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
+#define MASTER_LEFT/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
 #define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define USE_SERIAL_PD2
+
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 100
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+#define SPLIT_OLED_ENABLE
+#define OLED_TIMEOUT 90000
+
+#define SPLIT_ACTIVITY_ENABLE
+#define SPLIT_WPM_ENABLE
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN GP22
+#define I2C1_SCL_PIN GP23
+
+#define AUDIO_PIN GP28
+#define AUDIO_PWM_DRIVER PWMD6
+#define AUDIO_INIT_DELAY
+#define AUDIO_PWM_CHANNEL RP2040_PWM_CHANNEL_A
+#define STARTUP_SONG SONG(ODE_TO_JOY)
+
+#define ANALOG_JOYSTICK_Y_AXIS_PIN GP26
+#define ANALOG_JOYSTICK_X_AXIS_PIN GP27
+#define POINTING_DEVICE_INVERT_Y
+#define POINTING_DEVICE_INVERT_X
+
+
+
+#ifdef RGBLIGHT_ENABLE
+    #undef RGBLED_NUM
+	#define RGBLIGHT_LAYERS
+    #define RGBLED_NUM 54
+    #define RGBLIGHT_LIMIT_VAL 120
+    #define RGBLIGHT_HUE_STEP 10
+    #define RGBLIGHT_SAT_STEP 1
+    #define RGBLIGHT_VAL_STEP 17
+#endif
+
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
